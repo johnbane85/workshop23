de78320abff86433dbe388ee922b71b65234d03e
johnbane85 <junyu.siew@gmail.com>
2022-12-04 11:57:18 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/pafworkshop23/workshop23/controller/OrderDetailsRestController.java
@@ -0,0 +1,30 @@
+package sg.edu.nus.pafworkshop23.workshop23.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.nus.pafworkshop23.workshop23.models.OrderDetails;
+import sg.edu.nus.pafworkshop23.workshop23.service.OrderDetailsService;
+
+@RestController
+@RequestMapping(path = "/order/total", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrderDetailsRestController {
+
+  @Autowired
+  private OrderDetailsService odSvc;
+
+  @GetMapping(path = "{orderId}")
+  public ResponseEntity<String> getOrderDetailsWithDiscountedPrice(@PathVariable Integer orderId) {
+    OrderDetails ord = odSvc.getDiscountedOrderDetails(orderId);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(ord.toJSON().toString());
+  }
+}
